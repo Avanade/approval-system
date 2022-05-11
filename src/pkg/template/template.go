9893830a05c4f64c495d8e38a452a57c7016ffd5
Ismael Ibuan <ismael.r.ibuan@accenture.com>
2022-05-11 22:28:54 +0800

    refactor: Highlight current page on sidebar

--- src/pkg/template/template.go
+++ src/pkg/template/template.go
@@ -6,6 +6,7 @@ import (
 	"main/models"
 	session "main/pkg/session"
 	"net/http"
+	"strings"
 )
 
 // This parses the master page layout and the required page template.
@@ -20,7 +21,7 @@ func UseTemplate(w *http.ResponseWriter, r *http.Request, page string, pageData 
 	// Data on master page
 	var menu []models.TypMenu
 	menu = append(menu, models.TypMenu{Name: "Dashboard", Url: "/", IconPath: "/public/icons/dashboard.svg"})
-	menu = append(menu, models.TypMenu{Name: "Projects", Url: "/projects/my", IconPath: "/public/icons/projects.svg"})
+	menu = append(menu, models.TypMenu{Name: "Projects", Url: "/projects/new", IconPath: "/public/icons/projects.svg"})
 	menu = append(menu, models.TypMenu{Name: "Communities", Url: "/communities/my", IconPath: "/public/icons/communities.svg"})
 	menu = append(menu, models.TypMenu{Name: "Guidance", Url: "/guidance", IconPath: "/public/icons/guidance.svg"})
 	menu = append(menu, models.TypMenu{Name: "Approvals", Url: "/approvals/my", IconPath: "/public/icons/approvals.svg"})
@@ -29,7 +30,7 @@ func UseTemplate(w *http.ResponseWriter, r *http.Request, page string, pageData 
 	externalLinks = append(externalLinks, models.TypMenu{Name: "Tech Community Calendar", Url: "/#", IconPath: "/public/icons/calendar.svg"})
 	externalLinks = append(externalLinks, models.TypMenu{Name: "Stack Overflow at Avanade", Url: "/#", IconPath: "/public/icons/questionmark.svg"})
 	externalLinks = append(externalLinks, models.TypMenu{Name: "Open Innovation Meetup", Url: "/#", IconPath: "/public/icons/microphone.svg"})
-	masterPageData := models.TypHeaders{Menu: menu, ExternalLinks: externalLinks, Page: page}
+	masterPageData := models.TypHeaders{Menu: menu, ExternalLinks: externalLinks, Page: getUrlPath(r.URL.Path)}
 
 	data := models.TypPageData{
 		Header:  masterPageData,
@@ -40,4 +41,13 @@ func UseTemplate(w *http.ResponseWriter, r *http.Request, page string, pageData 
 		template.ParseFiles("templates/master.html",
 			fmt.Sprintf("templates/%v.html", page)))
 	return tmpl.Execute(*w, data)
+}
+
+func getUrlPath(path string) string {
+	p := strings.Split(path, "/")
+	if p[1] == "" {
+		return "Dashboard"
+	} else {
+		return strings.Title(p[1])
+	}
 }
