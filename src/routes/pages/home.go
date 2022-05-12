7d5b80d808ecae0c2ee5d6d737479564b5c34c3f
Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>
2022-05-12 18:05:15 +0800

    feat added github login information alert

--- src/routes/pages/home.go
+++ src/routes/pages/home.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	session "main/pkg/session"
 	template "main/pkg/template"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	template.UseTemplate(&w, r, "home", nil)
+	gitHubUser, err := session.GetGitHubUserData(w, r)
+	if err != nil {
+		return
+	}
+	template.UseTemplate(&w, r, "home", gitHubUser)
 }
