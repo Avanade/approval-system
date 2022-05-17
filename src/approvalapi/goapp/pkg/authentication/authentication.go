d6376d33d794df5d7de7bd57fc164117e62e69eb
Ismael Ibuan <ismael.r.ibuan@accenture.com>
2022-05-17 21:37:14 +0800

    feat: Approval System Go application with azure authentication

---
+++ src/approvalapi/goapp/pkg/authentication/authentication.go
@@ -0,0 +1,40 @@
+package authentication
+
+import (
+	"context"
+	"log"
+	"main/pkg/envvar"
+	"os"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+type Authenticator struct {
+	Provider *oidc.Provider
+	Config   oauth2.Config
+	Ctx      context.Context
+}
+
+func NewAuthenticator() (*Authenticator, error) {
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "https://login.microsoftonline.com/"+os.Getenv("tenantid")+"/v2.0")
+	if err != nil {
+		log.Printf("failed to get provider: %v", err)
+		return nil, err
+	}
+
+	conf := oauth2.Config{
+		ClientID:     os.Getenv("clientid"),
+		ClientSecret: os.Getenv("clientsecret"),
+		RedirectURL:  envvar.GetEnvVar("homeurl", "http://localhost:8080") + "/login/azure/callback",
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, oidc.ScopeOfflineAccess, "profile"},
+	}
+
+	return &Authenticator{
+		Provider: provider,
+		Config:   conf,
+		Ctx:      ctx,
+	}, nil
+}
---
+++ src/approvalapi/goapp/public/js/scripts.js
@@ -0,0 +1,63 @@
+// For showing/hiding sidebar on medium and small screens
+(function () {
+  let hideSidebar = document.getElementById("close-sidebar"),
+    sidebar = document.getElementById("sidebar");
+    shade = document.getElementById("shade")
+
+  moveToMain("pageTitle")
+
+  function moveToMain(id) {
+    let dom = document.getElementById(id)
+    let masterDom = document.getElementById(id + "Placeholder")
+    if(dom != null && masterDom != null){
+      masterDom.innerHTML = dom.innerHTML
+      dom.remove()
+    }
+  }
+
+  function toggleSidebar() {
+    let classesToApplyForSidebar = {
+      active: [],
+      inactive: ["transform", "-translate-x-full"],
+    },
+    classesToApplyForShade = {
+      active: [],
+      inactive: ["hidden"]
+    };
+
+    let isMenuActive = hideSidebar.getAttribute("data-menu-active") === "true";
+
+    if (isMenuActive) {
+      hideSidebar.setAttribute("data-menu-active", null);
+
+      sidebar.classList.remove(...classesToApplyForSidebar.active);
+      sidebar.classList.add(...classesToApplyForSidebar.inactive);
+
+      shade.classList.remove(...classesToApplyForShade.active);
+      shade.classList.add(...classesToApplyForShade.inactive)
+    } else {
+      hideSidebar.setAttribute("data-menu-active", true);
+
+      sidebar.classList.add(...classesToApplyForSidebar.active);
+      sidebar.classList.remove(...classesToApplyForSidebar.inactive);
+
+      shade.classList.add(...classesToApplyForShade.active);
+      shade.classList.remove(...classesToApplyForShade.inactive)
+    }
+  }
+
+  document.getElementById("close-sidebar").addEventListener("click", (e) => {
+    e.preventdefault;
+    toggleSidebar();
+  });
+
+  document.getElementById("show-sidebar").addEventListener("click", (e) => {
+    e.preventdefault;
+    toggleSidebar();
+  });
+
+  toggleSidebar();
+  setTimeout(() => {
+    sidebar.classList.add("duration-300");
+  }, 300);
+})();
