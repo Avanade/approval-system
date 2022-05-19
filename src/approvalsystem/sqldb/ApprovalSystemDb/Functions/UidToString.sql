dce994772e82f60e18b4ef376c805cbc186d64fa
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-05-19 22:34:38 +0800

    fixes on .sql

--- src/approvalsystem/sqldb/ApprovalSystemDb/Functions/UidToString.sql
+++ src/approvalsystem/sqldb/ApprovalSystemDb/Functions/UidToString.sql
@@ -1,15 +1,3 @@
--- ======================================================
--- Create Scalar Function Template for Azure SQL Database
--- ======================================================
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
--- =============================================
--- Author:      <Author, , Name>
--- Create Date: <Create Date, , >
--- Description: <Description, , >
--- =============================================
 CREATE FUNCTION UidToString
 (
     -- Add the parameters for the function here
