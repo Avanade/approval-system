ff884c609c95637c818d8cd6f9e84241aa3aa513
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-05-20 16:18:03 +0800

    feat: Select additional columns

--- src/approvalsystem/sqldb/ApprovalSystemDb/Stored Procedures/PR_Items_Select_ByApproverEmail.sql
+++ src/approvalsystem/sqldb/ApprovalSystemDb/Stored Procedures/PR_Items_Select_ByApproverEmail.sql
@@ -2,11 +2,13 @@ CREATE PROCEDURE [dbo].[PR_Items_Select_ByApproverEmail]
 	@ApproverEmail varchar(100)
 AS
 	SELECT
-		A.[Name] [Application],
-		AM.[Name] [Module],
-		[Subject], Body, DateSent,
-		DateResponded, IsApproved, ApproverRemarks, I.Created
+		dbo.UidToString(A.Id) [ApplicationId], A.[Name] [Application],
+		dbo.UidToString(AM.Id) [ApplicationModuleId], AM.[Name] [Module],
+		dbo.UidToString(I.Id) [ItemId], [Subject], Body, DateSent,
+		DateResponded, IsApproved, ApproverRemarks, I.Created,
+		T.ApproveText, T.RejectText
 	FROM Items I
 	INNER JOIN ApplicationModules AM ON I.ApplicationModuleId = AM.Id
 	INNER JOIN Applications A ON AM.ApplicationId = A.Id
+	INNER JOIN ApprovalTypes T ON T.Id = AM.ApprovalTypeId
 	WHERE I.ApproverEmail = @ApproverEmail
