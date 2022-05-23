169c44654d91ca984832e7e1ae946ed8ae7627bf
Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>
2022-05-23 13:28:41 +0800

    feat : save az user in azure sql

--- src/goapp/pkg/ghmgmtdb/ghmgmtdb.go
+++ src/goapp/pkg/ghmgmtdb/ghmgmtdb.go
@@ -1,7 +1,7 @@
 package ghmgmt
 
 import (
-	sql "main/pkg/sql"
+	"main/pkg/sql"
 	"os"
 )
 
@@ -13,10 +13,43 @@ func GetUsersWithGithub() interface{} {
 	return result
 }
 
-func ConnectDb() (*sql.DB) {
+func IsUserExist(userPrincipalName string) bool {
+	db := ConnectDb()
+	defer db.Close()
+
+	param := map[string]interface{}{
+		"UserPrincipalName": userPrincipalName,
+	}
+
+	result, _ := db.ExecuteStoredProcedureWithResult("PR_Users_IsExisting", param)
+
+	return result[0]["Result"] == 1
+}
+
+func InsertUser(userPrincipalName, givenName, surName, jobTitle, githubUser string) error {
+	db := ConnectDb()
+	defer db.Close()
+
+	param := map[string]interface{}{
+		"UserPrincipalName": userPrincipalName,
+		"GivenName":         givenName,
+		"SurName":           surName,
+		"JobTitle":          jobTitle,
+		"GithubUser":        githubUser,
+	}
+
+	_, err := db.ExecuteStoredProcedure("PR_Users_Insert", param)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ConnectDb() *sql.DB {
 	cp := sql.ConnectionParam{
 		ConnectionString: os.Getenv("GHMGMTDB_CONNECTION_STRING"),
-		}
+	}
 
 	db, _ := sql.Init(cp)
 
