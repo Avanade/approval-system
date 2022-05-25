5e7c755a20d2e0d267fa0258cca8f03ebc331b2d
Daryl Tejares <daryltejares@gmail.com>
2022-05-25 23:36:44 +0800

    CoOwner CB

--- src/goapp/routes/pages/projects/new.go
+++ src/goapp/routes/pages/projects/new.go
@@ -6,12 +6,14 @@ import (
 	githubAPI "main/pkg/github"
 	template "main/pkg/template"
 	"net/http"
+	db "main/pkg/ghmgmtdb"
 )
 
 func ProjectsNewHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		template.UseTemplate(&w, r, "projects/new", nil)
+		users := db.GetUsersWithGithub()
+		template.UseTemplate(&w, r, "projects/new", users)
 	case "POST":
 		var body models.TypNewProjectReqBody
 		err := json.NewDecoder(r.Body).Decode(&body)
