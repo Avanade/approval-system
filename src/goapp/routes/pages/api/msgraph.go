5291ca6b71dce88ffdeccc91252063ceee21fce1
Ismael Ibuan <ismael.r.ibuan@accenture.com>
2022-06-09 17:03:51 +0800

    feat: API to get all users from the active directory

---
+++ src/goapp/routes/pages/api/msgraph.go
@@ -0,0 +1,24 @@
+package route
+
+import (
+	"encoding/json"
+	"main/pkg/msgraph"
+	"net/http"
+)
+
+func GetAllUserFromActiveDirectory(w http.ResponseWriter, r *http.Request) {
+	users, err := msgraph.GetAllUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	jsonResp, err := json.Marshal(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonResp)
+}
