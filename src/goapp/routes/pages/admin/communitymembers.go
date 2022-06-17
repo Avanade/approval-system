57911d077162a36436a19622defd054898cd264e
Ismael Ibuan <ismael.r.ibuan@accenture.com>
2022-06-17 21:25:18 +0800

    feat: add check if user is admin

--- src/goapp/routes/pages/admin/communitymembers.go
+++ src/goapp/routes/pages/admin/communitymembers.go
@@ -1,10 +1,22 @@
 package routes
 
 import (
+	"main/pkg/session"
 	"main/pkg/template"
 	"net/http"
 )
 
 func ListCommunityMembers(w http.ResponseWriter, r *http.Request) {
-	template.UseTemplate(&w, r, "admin/communitymembers", nil)
+	// Check if user is an admin
+	isAdmin, err := session.IsUserAdmin(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if isAdmin {
+		template.UseTemplate(&w, r, "admin/communitymembers", nil)
+	} else {
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+	}
 }
