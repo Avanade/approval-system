b29ee18de1c0daafe5451df3bcfe4c3e973a3335
Ismael Ibuan <ismael.r.ibuan@accenture.com>
2022-06-17 21:31:25 +0800

    refactor: fix conflicts

--- src/goapp/routes/pages/admin/communitymembers.go
+++ src/goapp/routes/pages/admin/communitymembers.go
@@ -1,22 +1,10 @@
 package routes
 
 import (
-	"main/pkg/session"
 	"main/pkg/template"
 	"net/http"
 )
 
 func ListCommunityMembers(w http.ResponseWriter, r *http.Request) {
-	// Check if user is an admin
-	isAdmin, err := session.IsUserAdmin(w, r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if isAdmin {
-		template.UseTemplate(&w, r, "admin/communitymembers", nil)
-	} else {
-		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
-	}
+	template.UseTemplate(&w, r, "admin/communitymembers", nil)
 }
